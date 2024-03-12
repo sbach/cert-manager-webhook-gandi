0cdbfec7fdb402121f44c305a8edb9e59c27ffca
Stanislas Bach <sbach@0g.re>
2024-03-12 14:34:46 -0700

    add 'flake.nix'

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          curl
+          docker
+          envsubst
+          go_1_22
+          gnutar
+        ];
+      };
+    };
+}
